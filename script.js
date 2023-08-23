3ec818274f9d923b338dc5726be5715484a30d7d
isa <125407240+Rabelaqbv@users.noreply.github.com>
2023-08-23 15:17:49 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function green(){
-document.getElementById('myImage').src='2051440-small-removebg.png';
-}
-
-function transp(){
-document.getElementById('myImage').src='2051440-small-removebg-preview.png';
-}
