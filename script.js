efa2bc5a2a550d6afef0b2169fc764d05fedb9ff
isa <125407240+Rabelaqbv@users.noreply.github.com>
2023-08-23 15:06:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function green(){
+document.getElementById('myImage').src='2051440-small-removebg.png';
+}
+
+function transp(){
+document.getElementById('myImage').src='2051440-small-removebg-preview.png';
+}
